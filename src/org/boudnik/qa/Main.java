692955bfe8364802846c86d41cd6e6a283b3dc8d
aboudnik <aboudnik@3ca9a20c-4252-425e-a018-931ab5007d81>
2010-07-28 06:00:18 +0000

    DB

--- src/org/boudnik/qa/Main.java
+++ src/org/boudnik/qa/Main.java
@@ -4,18 +4,31 @@
 
 package org.boudnik.qa;
 
+import org.boudnik.better.sql.DB;
 import org.boudnik.better.sql.OBJ;
 import org.boudnik.better.sql.MetaData;
 import org.boudnik.qa.core.*;
+
+import java.net.PasswordAuthentication;
+import java.net.UnknownHostException;
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.SQLException;
 
 /**
  * @author shr
  * @since Aug 31, 2005 10:29:19 PM
  */
 public class Main {
-    public static void main(final String[] args) throws IllegalAccessException, InstantiationException {
+    public static void main(final String[] args) throws IllegalAccessException, InstantiationException, UnknownHostException, SQLException {
         MetaData metaData = new MetaData(OBJ.class, Foo.class, Zoo.class, Bar.class, Poo.class);
 //        MetaData.process(Boolean.class, OBJ.class, Foo.class, Zoo.class, Bar.class);
+
+        DB db = DB.open(DB.H2.class, "localhost", "test", new PasswordAuthentication("APP", "".toCharArray()));
+        final Connection connection = db.getConnection();
+        final DatabaseMetaData data = connection.getMetaData();
+        System.out.println(data.getDriverName() + " " + data.getDriverVersion());
+        System.out.println(data.getDatabaseProductVersion() + " " + data.getDatabaseProductVersion());
         metaData.print();
         final Foo foo = new Foo();
         foo.name.set("bla");
